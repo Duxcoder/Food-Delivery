98c6e814f469d7d85607f222af8a7b6214935e26
Duxcoder <iinegve_25@mail.ru>
2022-11-23 22:53:48 +0300

    add logic for searching

--- src/components/header/search/search.js
+++ src/components/header/search/search.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from "react";
 import styles from './style.module.sass'
 import {FiSearch} from 'react-icons/fi'
+import SearchResult from "./searchResult/searchResult";
 export default function Search(props) {
     const [click, setClick] = useState(false)
+    const [searchText, setSearchText] = useState('')
+
     const focusSearch = () => setClick(true)
     useEffect(() => {
         const closeDrop = e => {
@@ -17,10 +20,13 @@ export default function Search(props) {
             document.body.removeEventListener('click', closeDrop)
         }
     }, [])
-    return (
+    return (<>
         <div className={`${styles.search_wrapper} ${props.mobile ? styles.modeSearchMobile : click ? styles.modeSearchActive : styles.modeSearch }`}>
-            <input placeholder={props.placeholder} className={styles.search} onFocus={() => focusSearch()}></input>
+            <input placeholder={props.placeholder} className={styles.search} onChange={(item) => setSearchText(item.target.value)} onFocus={() => focusSearch()}></input>
             <FiSearch className={styles.icon_search}></FiSearch>
+        {click ? <SearchResult text = {searchText} /> : null}
+
         </div>
+        </>
     )
 }
---
+++ src/components/header/search/searchResult/searchResult.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import styles from './style.module.sass'
+import Data from "../../../../database";
+export default function SearchResult(props) {
+    const [elements, setElements] = useState()
+    useEffect(() => {
+       setElements(searching(props.text))
+    }, [props.text])
+    const searching = (text) => {
+        return Data.cards.map(item => {
+            if (text) {
+                return item.title.toLowerCase().includes(text.toLowerCase()) ? item : console.log()
+            }
+        }).filter(item => item !== undefined)
+    }
+
+    const renderResult = () => {
+        console.log(elements)
+        if (elements) {
+            return elements.map(item => {
+                return <a href="/">{item.title}</a>
+           })
+        }
+         
+    }
+
+
+    return (<><div className={styles.result_wrapper}>{renderResult()}
+    </div>
+    </>
+    )
+}
